d741a477419c9f2de13f53db71993946db33c407
VA <anandin02@gmail.com>
2022-04-17 11:37:24 +0530

    Initial Commit

---
+++ SwiftUI_InjectIII/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_InjectIII
+//
+//  Created by Vikash Anand on 17/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_InjectIII/SwiftUI_InjectIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InjectIIIApp.swift
+//  SwiftUI_InjectIII
+//
+//  Created by Vikash Anand on 17/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InjectIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
